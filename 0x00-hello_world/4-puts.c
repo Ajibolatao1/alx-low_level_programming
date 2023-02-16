382ab28a56e1e481132a41b099ccf051bef6a927
ajibolatao1 <taolawbal4@gmail.com>
2023-02-16 05:16:55 -0800

    Low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void)
- {
+{
 	/*This uses the puts funtion to print out the content*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- } 
+} 
