fb6518f0a03555306ddae52bee942964dec4752b
ajibolatao1 <taolawbal4@gmail.com>
2023-02-16 06:11:34 -0800

    Low level programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - print the data type
+ *
+ * Return: returning value
+ *
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of a int: %d byte(s)", sizeof(int));
+	printf("Size of a long: %d byte(s)", sizeof(long));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+
+}
