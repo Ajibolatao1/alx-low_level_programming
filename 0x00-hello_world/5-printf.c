6d3206dba9671fac7bbf7fd273f26cdfff7ba1c4
ajibolatao1 <taolawbal4@gmail.com>
2023-02-16 05:51:24 -0800

    Low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * printf - the main code
  *
- * Return - Always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
