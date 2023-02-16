7ff839ec3017029e365be59dde606a16f91fb54b
ajibolatao1 <taolawbal4@gmail.com>
2023-02-16 07:17:46 -0800

    Low level programming

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * prinftf - print thr function
+ *
+ * Result: print the result
+ */
+
+int main(void)
+{
+	printf("This is the main fiunction");
+	result (0);
+}
