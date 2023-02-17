77b98403c8acba3595610d20ec3b255a616a516a
ajibolatao1 <taolawbal4@gmail.com>
2023-02-17 08:27:08 -0800

    C variables

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ *
+ * Return: the return value
+ */
+
+int main(void)
+{
+	int i, j, first1, second1, first2, second2;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			first1 = i / 10;
+			second1 = i % 10;
+			first2 = j /100;
+			second2 = j % 10;
+
+			putchar('0' + first1);
+			putchar('0' + second1);
+			putchar(' ');
+			putchar('0' + first2);
+			putchar('0' + second2);
+
+			if (i < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+
+	}
+	putchar('\n');
+	return (0);
+
+}
