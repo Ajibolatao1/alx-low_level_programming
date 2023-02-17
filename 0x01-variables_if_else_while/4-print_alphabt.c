da1faa262a57dc6d62de8248c1885b779a568479
ajibolatao1 <taolawbal4@gmail.com>
2023-02-17 06:58:52 -0800

    C variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,18 @@
  *
  * putchar - to print letters 
  *
- * Return: the return value
+ * return: the return value
  */
 
 int main(void)
 {
-	for (char ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 		if (ch != 'q' && ch != 'e')
 		{
 			putchar(ch);
 		}
 	putchar('\n');
+	return (0);
 }
