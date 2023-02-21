21bee838c02320abfd9fb726dbd2f125a2737fc6
ajibolatao1 <taolawbal4@gmail.com>
2023-02-21 12:25:20 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,25 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
-		_putchar(44);
-		_putchar(' ');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
         {
-		_putchar(48);
-		_putchar(44);
-                _putchar(' ');
+		_putchar('0');
 		return (0);
         }
 	else if (n < 0)
 	{
-		_putchar(45);
-		_putchar(44);
-                _putchar(' ');
+		_putchar('-');
 		return (-1);
 	}
-
 
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to print the last digit of a number
+ *
+ * @r: the given varuable
+ *
+ * Return: The returned value
+ */
+
+int print_last_digit(int i)
+{
+	int i, n;
+
+	n = i % 10;
+
+	return(n);
+
+}
