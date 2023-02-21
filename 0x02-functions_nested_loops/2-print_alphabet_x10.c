64b5a0b49d7978e0703509a18fffb2519c2c6dee
ajibolatao1 <taolawbal4@gmail.com>
2023-02-21 04:00:00 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
 
 /** main - the entry point
  *
@@ -13,23 +14,4 @@ int main(void)
 	print_alphabet_x10();
 	return (0);
 
-}
-
-void print_alphabet_x10()
-{
-	int i, j;
-
-	i = 97;
-	j = 0;
-	while (j < 10)
-	{
-		while (i < 123)
-		{
-			putchar(i);
-			i++;
-		}
-		putchar("\n");
-		i = 97;
-		j++;
-	}	
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,3 +13,24 @@ void print_alphabet(void)
 	putchar("\n");
 
 }
+
+/* 2 */
+
+void print_alphabet_x10()
+{
+	int i, j;
+
+	i = 97;
+	j = 0;
+	while (j < 10)
+	{
+		while (i < 123)
+		{
+			putchar(i);
+			i++;
+		}
+		putchar("\n");
+		i = 97;
+		j++;
+	}
+}
