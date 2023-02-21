ab7a5caf4eea8f95ad013743027febd23704ad1e
ajibolatao1 <taolawbal4@gmail.com>
2023-02-21 11:49:40 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
-/** 
+/**
  * _isalpha - check if the inputted value is a character
+ *
+ * c - the inputted character
  *
  * Return: The returned value
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 91 && c >= 97 && c <= 123)
+	if (c >= 65 && c <= 91 || c >= 97 && c <= 123)
 	       return (1);
-	else 
-		return (0);	
+	else
+		return (0);
 
 }
