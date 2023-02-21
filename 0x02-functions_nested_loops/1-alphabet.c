a0b2ec51ce5c674a0a48fee1e4459ee1b559c238
ajibolatao1 <taolawbal4@gmail.com>
2023-02-21 05:50:24 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,8 @@
 #include "main.h"
-
 /** main - the entry point
  *
  * Return: the returned value
  */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-
-}
 
 void print_alphabet(void)
 {
@@ -21,3 +13,11 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 }
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+
+}
+
