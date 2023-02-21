b27149b03430dca2bd5d47e933910401f4019370
ajibolatao1 <taolawbal4@gmail.com>
2023-02-21 11:51:41 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,8 +10,10 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 91 || c >= 97 && c <= 123)
+	if (c >= 65 && c <= 91)
 	       return (1);
+	else if (c >= 97 && c <= 123)
+		return (1);
 	else
 		return (0);
 
