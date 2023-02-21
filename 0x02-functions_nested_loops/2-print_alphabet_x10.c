823554b7082e10dad32b115bcfc5a1ab8a728aa8
ajibolatao1 <taolawbal4@gmail.com>
2023-02-21 11:26:21 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,18 @@
  */
 
 void print_alphabet_x10(void);
-
-int main(void) 
 {
-	print_alphabet_x10();
-	return (0);
+	int i, j;
 
+	j = 0;
+	while (j < 10)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+		j++;
+	}
 }
