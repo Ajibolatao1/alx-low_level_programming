e3194144778c91b9901993fbbf246b072d9e1eba
ajibolatao1 <taolawbal4@gmail.com>
2023-02-22 12:27:46 -0800

    C funtions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,6 +16,7 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			printf("%d", n);
+			printf(", ");
 			n--;
 		}
 	}
@@ -24,6 +25,7 @@ void print_to_98(int n)
 		while (n < 98)
 		{
 			printf("%d", n);
+			printf(", ");
 			n++;
 		}
 	}
