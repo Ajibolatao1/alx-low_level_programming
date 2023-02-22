a76fd22ae3b695a54594238f014e07fa636ea3d9
ajibolatao1 <taolawbal4@gmail.com>
2023-02-22 10:54:30 -0800

    C funtions

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 multiplication table
+ *
+ * Return: the retrurned value
+ */
+
+void taimes_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(48);
+
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
+			_putchar(32);
+
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else 
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+
+		}
+		_putchar('\n');
+	
+	}
+
+}
