2b6f99f527540ff013598c4684566ca3d3ce602d
ajibolatao1 <taolawbal4@gmail.com>
2023-02-23 12:30:31 -0800

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: the passed in arguement
+ *
+ * Result: the returned result
+ */
+
+int _isupper(int c)
+{
+	if (c >= A && C <= Z)
+		return 1;
+	return 0;
+
+
+}
