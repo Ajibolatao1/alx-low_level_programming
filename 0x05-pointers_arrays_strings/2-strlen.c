6a374227378de6a9463ad750546e1d18a7d5c2ce
ajibolatao1 <taolawbal4@gmail.com>
2023-02-27 01:19:53 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strlen - function that returns the length of a string
  *
  * @s: the passed string
  *
+ * Return: the returned value
  */
 
 int _strlen(char *s)
 {
 	strlen(s);
+
+	return (0);
 
 
 }
