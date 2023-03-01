7eca434757e063f72dac793528875b17090193d8
ajibolatao1 <taolawbal4@gmail.com>
2023-03-01 10:38:33 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * *_strcat - string cat function
  * @dest: first passed in variable
  *
  * @src: second variable
@@ -18,12 +19,12 @@ char *_strcat(char *dest, char *src)
 	while (*ptr != '\0')
 		ptr++;
 
-	while(*src != '\0')
+	while (*src != '\0')
 		*ptr++ = *src++;
 
 	*ptr = '\0';
 
-	return dest;
+	return (dest);
 
 
 }
