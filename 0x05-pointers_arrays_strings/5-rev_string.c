e5c0edd452629a227e7f228aff1086f327511082
ajibolatao1 <taolawbal4@gmail.com>
2023-03-02 11:36:46 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,19 @@
  *
  * Return: void
  */
+
 void rev_string(char *s)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
+
+
 	for (i -= 1; i >= 0; i--)
-	{
 		_putchar(s[i]);
-	}
+
 	_putchar('\n');
 }
