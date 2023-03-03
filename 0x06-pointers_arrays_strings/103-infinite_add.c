680f6f6c5bba31e938024ec2b1c0dd3115332928
ajibolatao1 <taolawbal4@gmail.com>
2023-03-03 03:53:04 -0800

    C programming - pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,87 +1,85 @@
+#define FILL 100 /* fill with 'd' */
+void move_int_end_to_beg(char *str, int size, char fill);
 /**
- * infinite_add - adds two integers stored as strings
+ * infinite_add - adds two numbers
  *
- * @n1: first integer string to add
- * @n2: second integer string to add
- * @r: array to store resulting string in
- * @size_r: size of array r
+ * @n1: *char - first number
+ * @n2: *char - second number
+ * @r:  *char - buffer where result is stored
+ * @size_r: int - size of buffer
  *
- * Return: the summed string in r. If r is too small for the result,
- * return 0;
+ * Return: pointer to r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, index = 0, index2;
-	char *s1 = n1, *s2 = n2;
+	int tempSizeR = size_r;
+	int n1Length = 0, n2Length = 0, result = 0, carryStore = 0;
 
-	while (*s1 != 0)
-		s1++;
-	while (*s2 != 0)
-		s2++;
-	size_r--;
-	r[size_r] = 0;
-	s1--;
-	s2--;
-	while (s2 != n2 - 1 && s1 != n1 - 1)
+	while (*(n1 + n1Length) != '\0')
+		n1Length++;
+	while (*(n2 + n2Length) != '\0')
+		n2Length++;
+	/* account for 9999, 1, size_r:4, and extra space needed for '\0' */
+	if (n1Length > size_r - 2 || n2Length > size_r - 2)
+		return (0);
+	n1Length--, n2Length--; /* set element behind null byte */
+	while (size_r >= 0)
 	{
-		r[index] = *s2 - '0' + *s1 + carry;
-		carry = 0;
-		if (r[index] > '9')
-		{
-			carry++;
-			r[index] -= 10;
+		if (size_r == tempSizeR)
+		{ /* set last byte to end of string char or 'null byte' */
+			r[size_r--] = '\0';
+			continue;
 		}
-		index++;
-		s2--;
-		s1--;
-		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
-			return (0);
-	}
-	while (s1 != n1 - 1)
-	{
-		r[index] = *s1 + carry;
-		carry = 0;
-		if (r[index] > '9')
+		if (n1Length < 0 && n2Length < 0 && carryStore == 0)
 		{
-			carry = 1;
-			r[index] -= 10;
+			r[size_r--] = FILL;
+			continue; /* fill in rest with constants's */
 		}
-		s1--;
-		index++;
-		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
-			return (0);
-	}
-	while (s2 != n2 - 1)
-	{
-		r[index] = *s2 + carry;
-		carry = 0;
-		if (r[index] > '9')
+		else if (n1Length < 0 && n2Length < 0 && carryStore != 0)
+			result = carryStore;
+		else if (n1Length < 0)
+			result = n2[n2Length] + carryStore - '0';
+		else if (n2Length < 0)
+			result = n1[n1Length] + carryStore - '0';
+		else /* normal case */
 		{
-			carry = 1;
-			r[index] -= 10;
+			result = (n1[n1Length] - '0') + (n2[n2Length] - '0') + carryStore;
 		}
-		s2--;
-		index++;
-		if (size_r == index && (s2 != n2 - 1 || carry == 1))
-			return (0);
+		carryStore = 0;
+		if (result > 9)
+		{
+			carryStore = 1;
+			result = result - 10;
+		}
+		n1Length--, n2Length--;
+		r[size_r--] = result + '0';
 	}
-	if (carry == 1)
-	{
-		r[index] = '1';
-		r[index + 1] = 0;
-	}
-	else
-	{
-		r[index--] = 0;
-	}
-	index2 = 0;
-	while (index2 <= index)
-	{
-		carry = r[index];
-		r[index] = r[index2];
-		r[index2] = carry;
-		index--;
-		index2++;
-	}
+	move_int_end_to_beg(r, tempSizeR, FILL);
 	return (r);
+}
+/**
+ * move_int_end_to_beg - moves integer from end of str to beginning
+ *
+ * @arr: string to change
+ * @arrSize: size of string including null byte
+ * @fillChar: character used to fill string that isn't part being moved
+ *
+ * Return: always void
+ */
+void move_int_end_to_beg(char *arr, int arrSize, char fillChar)
+{
+	int i = 0;
+	int bufferPlacer = 0;
+	/* below loop moves elements to beginning of array */
+	while (i < arrSize)
+	{
+		if (arr[i] != fillChar)
+		{
+			arr[bufferPlacer++] = arr[i];
+			arr[i] = '\0';
+		}
+		else
+			arr[i] = '\0';
+		i++;
+	}
 }
