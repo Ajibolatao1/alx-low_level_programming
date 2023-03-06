9d007c7d0adedb79a447ba81cadb915d56a26384
ajibolatao1 <taolawbal4@gmail.com>
2023-03-06 05:08:51 -0800

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - the memset method
+ * @s: the returned meory
+ * @b: constant byte
+ * @n: the byte memory
+ *
+ * Return: The returned value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n-- > 0)
+		*ptr++ = c;
+
+	return (s);
+
+}
