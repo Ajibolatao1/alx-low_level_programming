864e3e6485ee9fda738d206b37ec410902001b00
ajibolatao1 <taolawbal4@gmail.com>
2023-03-06 06:13:43 -0800

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,17 +12,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
-	char *cdest, *csrc;
-
-	cdest = (char *) dest;
-	csrc = (char *) src;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		cdest[i] = csrc[i];
+		dest[i] = src[i];
 
 	return (dest);
-
-
-
 }
