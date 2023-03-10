cf2d738f675f3164a0d08a3936db72a43301c613
ajibolatao1 <taolawbal4@gmail.com>
2023-03-10 08:19:39 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,49 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main -  program that adds positive numbers.
+ * main - adds positive numbers
  * @argc: argument count
- * @argv: argument vector/array
- * Return: return 0
+ * @argv: argument vector for values
+ *
+ * Return: Always 0 (Success)
  */
-
-
 int main(int argc, char *argv[])
 {
-	int i, sum, numb;
+	int i, j, sum;
 
-	/* If no number is passed to the program, print 0, followed by a new line */
-	if (argv[1] == NULL)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	/* return 0 if it contains symbol */
-
-	for (i = 1; i < argc; i++)
-	{
-		if (atoi(argv[i]) == 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-
-	/* adds positive numbers */
 	sum = 0;
 
-	for (i = 1; i < argc; i++)
+	for (i = 1; i < argc ; i++)
 	{
-		numb = atoi(argv[i]);
-		sum = sum + numb;
+		for (j = 0; argv[i][j] != '\0' ; j++)
+		{
+			if (argv[i][j] < 47 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
 	}
-
-	   printf("%d\n", sum);
-
-
-
-
+	printf("%d\n", sum);
 	return (0);
 }
